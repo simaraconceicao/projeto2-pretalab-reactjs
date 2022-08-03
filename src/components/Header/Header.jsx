737bd026f861b9e071664d236e8cb94483832985
simaraconceicao <contato@simaraconceicao.com>
2022-08-02 21:18:22 -0300

    insere projeto

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="title-header">Meu Portfólio da PretaLab</h1>
+    </div>
+  )
+}
+
+export default Header
+
